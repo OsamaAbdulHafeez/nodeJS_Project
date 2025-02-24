7664654e1ebae521c0cde29ef16c984ec49ff241
OsamaAbdulHafeez <osamahafeez0987@gmail.com>
2025-02-24 13:32:17 +0500

    refactor database connection logic and update Vercel configuration for build process

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,10 +2,25 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.mongoURI);
+//     console.log("MongoDB Connect")
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export default connectDB;
+
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoURI);
-    console.log("MongoDB Connect")
+    mongoose.connect(process.env.mongoURI).then(() => {
+      console.log("MongoDB Connected Successfully");
+  }).catch((err) => {
+      console.error("MongoDB Connection Failed:", err);
+  });
   } catch (error) {
     console.log(error);
   }
