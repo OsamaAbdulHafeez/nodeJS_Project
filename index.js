bdaba9f0e7c690211e8f3bd621596e53e730aff8
OsamaAbdulHafeez <osamahafeez0987@gmail.com>
2025-02-24 14:25:39 +0500

    refactor database connection and improve API response handling

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './utils/connectDB.js'
-import mongoose from 'mongoose'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./utils/connectDB.js";
+import mongoose from "mongoose";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-const PORT = process.env.PORT
-connectDB()
+const PORT = process.env.PORT;
+connectDB();
 
-app.use(express.json())
+app.use(express.json());
 
+app.get("/", async (req, res) => {
+  const result = await connectDB();
+  if (result) {
+    console.log(result, "result");
+    return res.send("Hello World");
+  }
+});
 
-app.get("/",(req,res)=>{
-    return res.send("Hello World")
-})
-
-app.get('/api/getCollections', async(req,res)=>{
-    try {
-        if (mongoose.connection.readyState !== 1) {
-            await mongoose.connect(process.env.MONGO_URI);
-        }
-        const collections = await mongoose.connection.db.listCollections().toArray();
-        return res.status(200).send({
-            status:true,
-            error:false,
-            message:"Get All Tables",
-            data:collections
-        })
-    } catch (error) {
-        console.log(error)
+app.get("/api/getCollections", async (req, res) => {
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(process.env.MONGO_URI);
     }
-})
+    const collections = await mongoose.connection.db
+      .listCollections()
+      .toArray();
+    return res.status(200).send({
+      status: true,
+      error: false,
+      message: "Get All Tables",
+      data: collections,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Server is Connect is ${PORT}`)
-})
+app.listen(PORT, (req, res) => {
+  console.log(`Server is Connect is ${PORT}`);
+});
--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,25 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.mongoURI);
-//     console.log("MongoDB Connect")
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export default connectDB;
-
-
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.mongoURI).then(() => {
-      console.log("MongoDB Connected Successfully");
-  }).catch((err) => {
-      console.error("MongoDB Connection Failed:", err);
-  });
+    await mongoose.connect(process.env.mongoURI);
+    console.log("MongoDB Connect")
+    return true
   } catch (error) {
     console.log(error);
   }
